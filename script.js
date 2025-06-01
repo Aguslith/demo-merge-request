bc8a5e50e35c2587afbf05168364f99cefe52c5f
Agustin <herreraagustin7777@gmail.com>
2025-05-31 21:17:24 -0300

    agreguè otro participante

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// 🚀 Agregá tu nombre acá como una constante
-// Ejemplo:
-const alumno = "Brenda";
-console.log("Participante:", alumno);
-
 // Tu turno 👇
 const alumno2 = "Agustin";
 console.log("Participante:", alumno2);
